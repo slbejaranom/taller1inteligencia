ed7681e6e11a2c4040c4cd3ab1adfb498b915183
Sebastian Bejarano <slbejaranom@gmail.com>
2022-09-12 19:27:00 -0500

    First commit

---
+++ perceptron_taller.m
@@ -0,0 +1,8 @@
+function [salida] = perceptron_taller(entradas, pesos, polarizacion, umbral_step)
+%PERCEPTRON Genera una neurona con su salida
+%   A partir del modelo matemático de una neurona se generará la salida a
+%   partir de dos entradas usando
+%   y = w1*x1 + w2*x2 + ... + wn*xn + w0
+%   Las entradas deben ir como vector y los pesos como vectors columna
+    salida = (entradas*pesos + polarizacion > umbral_step)*1;
+end
